13e22a1f8c573880e0d1d4edbf03ba1f3e10a075
Jeroen Burgerhout <jeroen@burgerhout.org>
2019-11-18 14:46:34 +0100

    Added copyright

--- Chapter 02/Export-StartMenuLayout.ps1
+++ Chapter 02/Export-StartMenuLayout.ps1
@@ -1,4 +1,25 @@
-﻿# Export Start Menu layout on Windows 10 version 1607, 1703 an 1803
+﻿<#
+.SYNOPSIS
+  Export Start Menu Layout on Windows 10
+
+.DESCRIPTION
+  This PowerShell command belongs to chapter 2 of the MD-100 exam guide.
+
+.INPUTS
+  None
+
+.OUTPUTS
+  New local user account
+
+.NOTES
+  Version:        1.0
+  Author:         Jeroen Burgerhout
+  Creation Date:  17-11-2019
+  Purpose/Change: Initial script development
+  
+#>
+
+# Export Start Menu layout on Windows 10 version 1607, 1703 an 1803
 Export-StartLayout -Path X:\StartMenuLayout.xml
 
 # Export Start Menu layout on Windows 10 version 1809 and higher
---
+++ Chapter 03/NewLocalUserAccount.ps1
@@ -0,0 +1,23 @@
+﻿<#
+.SYNOPSIS
+  Create new local user
+
+.DESCRIPTION
+  This PowerShell command belongs to chapter 3 of the MD-100 exam guide.
+
+.INPUTS
+  Type a password for the user account
+
+.OUTPUTS
+  New local user account
+
+.NOTES
+  Version:        1.0
+  Author:         Jeroen Burgerhout
+  Creation Date:  18-11-2019
+  Purpose/Change: Initial script development
+  
+#>
+
+$Password = Read-Host -AsSecureString
+New-LocalUser -Name Jeroen -Password $Password -Description "Personal account" -FullName "Jeroen Burgerhout"
