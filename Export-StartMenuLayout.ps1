ade39be46f78e7b8c31d20a5484213fd8487b648
Jeroen Burgerhout <jeroen@burgerhout.org>
2019-11-18 10:37:56 +0100

    Create Export-StartMenuLayout.ps1

---
+++ Export-StartMenuLayout.ps1
@@ -0,0 +1,5 @@
+﻿# Export Start Menu layout on Windows 10 version 1607, 1703 an 1803
+Export-StartLayout -Path X:\StartMenuLayout.xml
+
+# Export Start Menu layout on Windows 10 version 1809 and higher
+Export-StartLayout -UseDesktopApplicationID -Path X:\ StartMenuLayout.xml
