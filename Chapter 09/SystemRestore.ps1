1ccd875940722c31461c3df56cfb3cfddde3489f
Jeroen Burgerhout <jeroen@burgerhout.org>
2020-02-26 15:28:21 +0100

    Create SystemRestore.ps1
    initial commit

---
+++ Chapter 09/SystemRestore.ps1
@@ -0,0 +1,28 @@
+<#
+.SYNOPSIS
+  Example of the some System Restore commands.
+
+.DESCRIPTION
+  In this PowerShell you will find the some System Restore commands that belongs to chapter 9 of the MD-100 exam guide.
+
+.INPUTS
+  <none>
+
+.NOTES
+  Version:        1.0
+  Author:         Jeroen Burgerhout
+  Creation Date:  26-02-2020
+  Purpose/Change: Initial script development
+  
+#>
+# Enable System Restore on the C: drive of the local computer
+Enable-ComputerRestore -Drive "C:\"
+
+# Disable System Restore on the C: drive of the local computer
+Disable-ComputerRestore -Drive "C:\"
+
+# Gets the restore points of the local computer
+Get-ComputerRestorePoint
+
+# Creates a System Restore point on the local computer
+Checkpoint-Computer -Description "This is a system restore point"
