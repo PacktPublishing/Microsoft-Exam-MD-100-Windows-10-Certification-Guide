62c8ec6ee30b0dafc6c4213c630fec33e7463012
Jeroen Burgerhout <jeroen@burgerhout.org>
2020-02-26 15:38:55 +0100

    Create EnablingPowerShellRemoting.ps1
    commit

---
+++ Chapter 08/EnablingPowerShellRemoting.ps1
@@ -0,0 +1,28 @@
+<#
+.SYNOPSIS
+  Example of the some commands which you can use to enable PowerShell remoting.
+
+.DESCRIPTION
+  In this PowerShell you will find the some commands which you can use to enable PowerShell remoting that belongs to chapter 8 of the MD-100 exam guide.
+
+.INPUTS
+  <none>
+
+.NOTES
+  Version:        1.0
+  Author:         Jeroen Burgerhout
+  Creation Date:  26-02-2020
+  Purpose/Change: Initial script development
+  
+#>
+# Check to see if the WinRM is enabled
+Get-Service WinRM
+
+# Configuring PowerShell for remote management
+Enable-PSRemoting -Force
+
+# Add a remote computer to the Local Computer Trusted Hosts list
+winrm.cmd s winrm/config/client '\@{TrustedHosts="RemoteComputerName"}'
+
+# Verifiy if the WinRM service is up and running on the remote host
+winrm.cmd quickconfig
